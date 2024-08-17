b104410e85707b12f13260ce00658941f0e88adb
Jake O'Reilly <78006039+JakeDaSpud@users.noreply.github.com>
2024-08-17 15:01:43 +0100

    :sparkles: DONE MOVEMENT YAYYY SMOOTH TURNING

--- Scripts/player_grounded.gd
+++ Scripts/player_grounded.gd
@@ -3,7 +3,7 @@ extends CharacterBody3D
 @export var _move_speed : float = 5.0;
 @export var _acceleration_speed : float = 5.0;
 
-@export var _turning_speed : float = 0.1;
+@export var _turning_speed : float = 10;
 @export var _smooth_turn : bool = true;
 
 func _physics_process(delta):
@@ -14,26 +14,26 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back");
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized();
+	var direction = Vector3(input_dir.x, 0, input_dir.y).normalized();
+	
+	var target_direction : Vector3 = direction;
+	var target_rotation : Basis = Transform3D().looking_at(target_direction, Vector3.UP).basis;
+	var current_rotation : Basis = self.global_basis;
 	
 	if direction:
 		velocity.x = direction.x * _move_speed;
 		velocity.z = direction.z * _move_speed;
 		
-		var target_direction : Vector3 = self.global_position + direction;
-		target_direction.y = 0;
-		
-		if _smooth_turn:
-			# Slerp to new direction
-			self.rotation = self.rotation;
-		
-		else:
+		if !_smooth_turn:
 			# Snap to the new direction
-			self.look_at(target_direction, Vector3.UP);
+			self.look_at(target_direction);
+		else:
+			var smooth_rotation : Basis = current_rotation.slerp(target_rotation, _turning_speed * delta);
+			self.global_basis = smooth_rotation;
 	
 	else:
-		velocity.x = move_toward(velocity.x, 0, _move_speed)
-		velocity.z = move_toward(velocity.z, 0, _move_speed)
+		velocity.x = move_toward(velocity.x, 0, _acceleration_speed);
+		velocity.z = move_toward(velocity.z, 0, _acceleration_speed);
 	
 	move_and_slide()
 	
